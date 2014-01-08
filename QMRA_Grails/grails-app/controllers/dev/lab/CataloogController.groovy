8a92743a5dd6ad6b19829713cc817e17ae512bde
stefanlivens <stefan.livens@gmail.com>
2014-01-08 05:24:51 -0800

    demo stuff

--- QMRA_Grails/grails-app/controllers/dev/lab/CataloogController.groovy
+++ QMRA_Grails/grails-app/controllers/dev/lab/CataloogController.groovy
@@ -1,12 +1,33 @@
-
-
 package dev.lab
 
 import dev.lab.Boek
+import grails.converters.JSON
 
 class CataloogController {
 
+    BooksService booksService
+
     def index() {
-		[cataloog:Boek.findAll()]
-	}
+        render (view:'index',model:[cataloog:Boek.findAll()])
+    }
+
+    def template() {
+        render (view:'template',model:[cataloog:Boek.findAll()])
+    }
+
+    def string(){
+        render booksService.cataloog    as JSON
+    }
+
+    def moresense(){
+        render booksService.cataloog*.description()    as JSON
+    }
+
+    def reuse() {
+    }
+
+    def reuse_part(){
+        [cataloog:booksService.cataloog]
+    }
+
 }
