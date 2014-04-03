98369ad05c5a7aacc82830ac612ee0db4ce5712d
Marco Kortooms <marco.kortooms@atos.net>
2014-04-03 15:34:05 +0200

    Toevoegen testveldMK3

--- QMRA_Grails/grails-app/domain/dev/lab/qmra/Critical_Condition.groovy
+++ QMRA_Grails/grails-app/domain/dev/lab/qmra/Critical_Condition.groovy
@@ -16,6 +16,7 @@ class Critical_Condition {
 	
 	String testveldMK1
 	String testveldMK2
+	String testveldMK3
 	String testveldTJ1
 	String testveldTJ2
 	
@@ -35,6 +36,7 @@ class Critical_Condition {
 		
 		testveldMK1()
 		testveldMK2()
+		testveldMK3()
 		testveldTJ1()
 		testveldTJ2()
     }
