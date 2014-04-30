2f1473cacbc0520591b06ab21dac7d741f192e6c
stefanlivens <stefan.livens@gmail.com>
2014-04-30 10:34:18 +0200

    defaults for long = 0

--- QMRA_Grails/grails-app/domain/dev/lab/qmra/Chlorine_Constant.groovy
+++ QMRA_Grails/grails-app/domain/dev/lab/qmra/Chlorine_Constant.groovy
@@ -2,18 +2,16 @@ package dev.lab.qmra
 
 class Chlorine_Constant {
 
-	//TODO: implement me! see Spring/../qmra.script for more info
-
     double chlorineConcentration
 	double temperature    
-    double a
+    double a 
 	double b
     	
     static constraints = {
-        chlorineConcentration(nullable:false,blank:false) 
-		temperature(nullable:false,blank:false)
-		a(nullable:true)
-		b(nullable:true)
+        chlorineConcentration() 
+		temperature()
+		a()
+		b()
     }
 }
 
