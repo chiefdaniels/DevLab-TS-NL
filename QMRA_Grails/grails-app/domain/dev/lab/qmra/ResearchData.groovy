50fc27490f694aa5671a048ace6f46610103bdfb
stefanlivens <stefan.livens@gmail.com>
2014-04-30 10:04:33 +0200

    now the researchdata can be cretaed in the bootstrap.groovy ! default zijn de velden niet nullable en niet blank, maw verplicht dus!

--- QMRA_Grails/grails-app/domain/dev/lab/qmra/ResearchData.groovy
+++ QMRA_Grails/grails-app/domain/dev/lab/qmra/ResearchData.groovy
@@ -30,27 +30,27 @@ class ResearchData {
 		researchId(blank:false)
 		year(blank:false)
 		organismType(nullable:false,blank:false)
-		orgainsm()
+		orgainsm(nullable:true,blank:true) /*changed*/
 		organismTarget(blank:false)
 		processType(nullable:false,blank:false)
-		filtrationType()
-		disinfectionType()
+		filtrationType(nullable:true,blank:true) /*changed*/
+		disinfectionType(nullable:true,blank:true) /*changed*/
 		publicationType(nullable:false,blank:false)
 		publicationDescription(nullable:false,blank:false)
 		publicationAuthor(nullable:false,blank:false)
 		publicationYear(nullable:false,blank:false)
 		
-		avg_reduction()
-		avg_stddev()
-		sampleCount()
-		virus_studyCount()
-		virus_sampleCount()
-		virus_uvType()
-		virus_uvFluence()
-		virus_inctmax()
-		virus_kValue()
-		virus_cl95()
-		virus_r2()
+		avg_reduction(nullable:true,blank:true) /*changed*/
+		avg_stddev(nullable:true,blank:true) /*changed*/
+		sampleCount(nullable:true,blank:true) /*changed*/
+		virus_studyCount(nullable:true,blank:true) /*changed*/
+		virus_sampleCount(nullable:true,blank:true) /*changed*/
+		virus_uvType(nullable:true,blank:true) /*changed*/
+		virus_uvFluence(nullable:true,blank:true) /*changed*/
+		virus_inctmax(nullable:true,blank:true) /*changed*/
+		virus_kValue(nullable:true,blank:true) /*changed*/
+		virus_cl95(nullable:true,blank:true) /*changed*/
+		virus_r2(nullable:true,blank:true) /*changed*/
 	}
 
 }
