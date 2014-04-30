eb13191ef0f4d64c24cffc5863277725a9b295f7
stefanlivens <stefan.livens@gmail.com>
2014-04-30 10:42:04 +0200

    questionaire

--- QMRA_Grails/grails-app/controllers/dev/lab/qmra/QuestionaireController.groovy
+++ QMRA_Grails/grails-app/controllers/dev/lab/qmra/QuestionaireController.groovy
@@ -1,5 +1,8 @@
 package dev.lab.qmra
 
+import dev.lab.qmra.ResearchData
+import dev.lab.qmra.enums.*
+
 class QuestionaireController {
 
 	static allowedMethods = [result: ["POST","GET"]]
@@ -13,7 +16,16 @@ class QuestionaireController {
 	
     def result() { 
 		println params
+		def view = 'result'
+		if (params.OrganismType == 'Bacteria'){
+			forward action:'bacteria'
+		} else {
+			def resdata=ResearchData.findByOrganismType(params.OrganismType)
+			render view:view,model:[answer:params.OrganismType?:'niets!',resdata:resdata]
+		}
+	}
+	
+	def bacteria(){
 		
-		[answer:params.test?:'niets!']
 	}
 }
